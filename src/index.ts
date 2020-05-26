88541d8b596f3c36266b34ebe6fc90b235256712
Max Goedjen <max.goedjen@gmail.com>
2020-05-26 00:35:45 -0700

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import { Annotation, AnnotationType, AnnotationLocation } from "./parser/parser";
+import { XcodeBuildParser } from "./parser/xcodebuild";
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const log = core.getInput('log');
+  console.log(`Hello ${log}!`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
