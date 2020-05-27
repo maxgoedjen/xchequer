933bd23746a96d8a20953afee928d735610d2f22
Max Goedjen <max.goedjen@gmail.com>
2020-05-26 23:02:40 -0700

    Test two

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Annotation, AnnotationType, AnnotationLocation } from "./parser/parser";
-import { XcodeBuildParser } from "./parser/xcodebuild";
-import { Annotator } from "./github/annotator";
+// import { Annotation, AnnotationType, AnnotationLocation } from "./parser/parser";
+// import { XcodeBuildParser } from "./parser/xcodebuild";
+// import { Annotator } from "./github/annotator";
 
 console.log("::warning file=src/index.ts,line=1,col=5::Test");
