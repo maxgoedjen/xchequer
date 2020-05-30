cf82ce6a3aed52033741d3c0ee075aa99260a918
Max Goedjen <max.goedjen@gmail.com>
2020-05-30 14:21:42 -0700

    .

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import { Annotation, AnnotationType, AnnotationLocation } from "./parser/parser";
 import { XcodeBuildParser } from "./parser/xcodebuild";
 import { Annotator } from "./github/annotator";
+import { Parameters } from "./github/parameters";
 import { readFileSync } from 'fs';
-
 
 const parser = new XcodeBuildParser();
 const annotator = new Annotator();
-const text = readFileSync('./build.log', 'utf-8');
+const params = new Parameters();
+const text = readFileSync(params.getInput('log'), 'utf-8');
+
 const errors = parser.parse(AnnotationType.Error, text);
 for (let entry of errors) {
   annotator.createAnnotation(entry);
 }
 const warnings = parser.parse(AnnotationType.Warning, text);
 for (let entry of warnings) {
+  annotator.createAnnotation(entry);
+}
+const runtime = parser.parse(AnnotationType.RuntimeFailure, text);
+for (let entry of runtime) {
   annotator.createAnnotation(entry);
 }
